b471b8649497445fca97a6fb341642c19c3af412
Anton Deguet <anton.deguet@jhu.edu>
2019-02-22 10:43:43 -0500

    first commit

---
+++ components/code/mtsSocketStreamer.cpp
@@ -0,0 +1,127 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Peter Kazanzides, Anton Deguet
+  Created on: 2013-12-02
+
+  (C) Copyright 2013-2019 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#include <sawSocketStreamer/mtsSocketStreamer.h>
+#include <cisstMultiTask/mtsInterfaceProvided.h>
+#include <cisstMultiTask/mtsInterfaceRequired.h>
+#include <cisstParameterTypes/prmPositionCartesianGet.h>
+#include <cisstParameterTypes/prmEventButton.h>
+
+CMN_IMPLEMENT_SERVICES_DERIVED_ONEARG(mtsSocketStreamer, mtsTaskPeriodic, mtsTaskPeriodicConstructorArg);
+
+void mtsSocketStreamer::Init(void)
+{
+    SocketConfigured = false;
+    Clutch = false;
+    Coag = false;
+
+    mtsInterfaceProvided * provided = AddInterfaceProvided("Configuration");
+    if (provided) {
+        provided->AddCommandWrite(&mtsSocketStreamer::SetDestination, this, "SetDestination");
+    }
+    mtsInterfaceRequired * required = AddInterfaceRequired("Robot");
+    if (required) {
+        required->AddFunction("GetPositionCartesian", GetPositionCartesian);
+        required->AddFunction("GetGripperPosition", GetGripperPosition);
+    }
+    required = AddInterfaceRequired("Clutch");
+    if (required) {
+        required->AddEventHandlerWrite(&mtsSocketStreamer::EventHandlerManipClutch, this, "Button");
+    }
+    required = AddInterfaceRequired("Coag");
+    if (required) {
+        required->AddEventHandlerWrite(&mtsSocketStreamer::EventHandlerCoag, this, "Button");
+    }
+}
+
+mtsSocketStreamer::~mtsSocketStreamer()
+{
+}
+
+void mtsSocketStreamer::Configure(const std::string &ipPort)
+{
+    SetDestination(ipPort);
+}
+
+void mtsSocketStreamer::Startup(void)
+{
+}
+
+void mtsSocketStreamer::Run(void)
+{
+    ProcessQueuedCommands();
+    ProcessQueuedEvents();
+
+    if (SocketConfigured) {
+        // Packet format (9 doubles): buttons (clutch, coag), gripper, x, y, z, q0, qx, qy, qz
+        // For the buttons: 0=None, 1=Clutch, 2=Coag, 3=Both
+        double packet[9];
+        if (Clutch) {
+            packet[0] = 1.0;
+        } else {
+            packet[0] = 0.0;
+        }
+        if (Coag)
+            packet[0] += 2.0;
+        GetGripperPosition(packet[1]);
+        prmPositionCartesianGet posCart;
+        GetPositionCartesian(posCart);
+        vct3 pos = posCart.Position().Translation();
+        packet[2] = pos.X();
+        packet[3] = pos.Y();
+        packet[4] = pos.Z();
+        vctQuatRot3 qrot(posCart.Position().Rotation());
+        packet[5] = qrot.W();
+        packet[6] = qrot.X();
+        packet[7] = qrot.Y();
+        packet[8] = qrot.Z();
+        Socket.Send((char *)packet, sizeof(packet));
+    }
+}
+
+void mtsSocketStreamer::Cleanup(void)
+{
+    Socket.Close();
+}
+
+void mtsSocketStreamer::SetDestination(const std::string &ipPort)
+{
+    size_t colon = ipPort.find(':');
+    if (colon == std::string::npos) {
+        CMN_LOG_CLASS_RUN_ERROR << "SetDestination: invalid address:port " << ipPort << std::endl;
+    } else {
+        unsigned short port;
+        if ((sscanf(ipPort.c_str() + colon + 1, "%hu", &port) != 1)) {
+            CMN_LOG_CLASS_RUN_ERROR << "SetDestination: invalid port " << ipPort << std::endl;
+
+        } else {
+            Socket.SetDestination(ipPort.substr(0, colon), port);
+            SocketConfigured = true;
+        }
+    }
+}
+
+void mtsSocketStreamer::EventHandlerManipClutch(const prmEventButton &button)
+{
+    Clutch = (button.Type() == prmEventButton::PRESSED);
+}
+
+void mtsSocketStreamer::EventHandlerCoag(const prmEventButton &button)
+{
+    Coag = (button.Type() == prmEventButton::PRESSED);
+}
---
+++ components/include/sawSocketStreamer/mtsSocketStreamer.h
@@ -0,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s):  Peter Kazanzides, Anton Deguet
+  Created on: 2013-12-02
+
+  (C) Copyright 2013-2019 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+
+#ifndef _mtsSocketStreamer_h
+#define _mtsSocketStreamer_h
+
+#include <cisstOSAbstraction/osaSocket.h>
+#include <cisstMultiTask/mtsTaskPeriodic.h>
+
+#include <cisstMultiTask/mtsForwardDeclarations.h>
+class prmEventButton;
+
+// Always include last!
+#include <sawSocketStreamer/sawSocketStreamerExport.h>
+
+class CISST_EXPORT mtsSocketStreamer: public mtsTaskPeriodic
+{
+    CMN_DECLARE_SERVICES(CMN_DYNAMIC_CREATION_ONEARG, CMN_LOG_ALLOW_DEFAULT);
+
+ public:
+    /*! Constructor
+        \param name Name of the component
+        \param period Period in seconds
+    */
+    inline mtsSocketStreamer(const std::string & name, double period):
+        mtsTaskPeriodic(name, period),
+        Socket(osaSocket::UDP)
+        {
+            Init();
+        }
+
+    inline mtsSocketStreamer(const mtsTaskPeriodicConstructorArg & arg):
+        mtsTaskPeriodic(arg),
+        Socket(osaSocket::UDP)
+        {
+            Init();
+        }
+    
+    /*! Destructor */
+    virtual ~mtsSocketStreamer();
+
+    /*! Configure: set IP address and port number
+        \param ipPort IP address and port number, separated by ':'
+    */
+    void Configure(const std::string & filename);
+    void Startup(void);
+    void Run(void);
+    void Cleanup(void);
+
+ protected:
+
+    void Init(void);
+    
+    osaSocket Socket;
+    bool SocketConfigured;
+
+    mtsFunctionRead GetPositionCartesian;
+    mtsFunctionRead GetGripperPosition;
+    bool Clutch;
+    bool Coag;
+
+    void SetDestination(const std::string &ipPort);
+    void EventHandlerManipClutch(const prmEventButton &button);
+    void EventHandlerCoag(const prmEventButton &button);
+
+
+};
+
+CMN_DECLARE_SERVICES_INSTANTIATION(mtsSocketStreamer)
+
+#endif // _mtsSocketStreamer_h
